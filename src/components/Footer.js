dc0a81200ddddf8a2df3c0b29ec6f12e753f439d
Matías Oveja Smith <matias.ovejasmith@gmail.com>
2017-01-05 17:40:36 -0300

    Update contact info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,12 @@ class Footer extends Component {
   render () {
     return (
       <div className='App-footer'>
-        <p className='App-footer-text'> Developed by </p>
+        <p className='App-footer-text'> WebTools 1 </p>
         <a target='_black' href='https://ar.linkedin.com/in/movejasmith' >
           <strong> Matias Oveja Smith </strong>
+        </a> <br></br>
+        <a target='_black' href='https://ar.linkedin.com/in/leo-rubino-aa4131125' >
+          <strong> Leo Rubino </strong>
         </a>
       </div>
 
